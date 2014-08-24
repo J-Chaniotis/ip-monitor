6f8e4fc44750be41d6c7663f0de2db11d0eabc11
J.Chaniotis <J.k.chaniotis@gmail.com>
2014-08-25 00:23:36 +0300

    improved tests and watcher. should be ready soon

--- test/test.watcher.js
+++ test/test.watcher.js
@@ -5,33 +5,29 @@ var expect = require('chai').expect;
 var Watcher = require('../lib/watcher').Watcher;
 
 var config = {
-    polling: 1000
+    polling: 100
 };
 
 // Hackish stub
 var extIPStub = function () {
-    var fn;
+    var args;
     return {
         getIP: function (cb) {
-            fn = cb;
+            cb.apply(cb, args);
         },
-        yield: function () {
-            if (!fn) {
-                throw new Error('cant yield');
-            }
-            fn.apply(fn, arguments);
-            fn = null;
+        set: function () {
+            args = arguments;
         }
     };
 };
 
 // Hackish invocation counter
 var counter = function (maxCount, done) {
-    var invocations = 0;
     return {
+        invocations: 0,
         increment: function (times) {
-            invocations += times || 1;
-            if(invocations === maxCount) {
+            this.invocations += times || 1;
+/* JavaScript */if (this.invocations === maxCount) {
                 done();
             }
         }
@@ -50,29 +46,34 @@ describe('watcher.js test', function () {
 
 
     it('sould emit "start" when watcher.start() is called for the first time', function (done) {
+        extIP.set(null, '10.10.10.10');
+
         watcher.on('start', function (ip) {
             expect(ip).to.equal('10.10.10.10');
             done();
         });
 
         watcher.start();
-        extIP.yield(null, '10.10.10.10');
+
     });
 
 
     it('sould emit "error" when extIP yields an error', function (done) {
+        extIP.set('Booom', null);
+
         watcher.on('error', function (err) {
             expect(err).to.equal('Booom');
             done();
         });
 
         watcher.start();
-        extIP.yield('Booom', null);
+
     });
 
     it('sould emit "ipChanged" when watcher.getIP() is called for the first time', function (done) {
 
         var countTwo = counter(2, done);
+        extIP.set(null, '10.10.10.10');
 
         watcher.on('ipChanged', function (prevIP, IP) {
             expect(IP).to.equal('10.10.10.10');
@@ -85,10 +86,13 @@ describe('watcher.js test', function () {
             countTwo.increment();
         });
 
-        extIP.yield(null, '10.10.10.10');
+
     });
 
     it('sould emit "stop" when watcher.stop() is called', function (done) {
+
+        extIP.set(null, '10.10.10.10');
+
         watcher.on('stop', function () {
             done();
         });
@@ -96,8 +100,28 @@ describe('watcher.js test', function () {
             watcher.stop();
         });
         watcher.start();
-        extIP.yield(null, '10.10.10.10');
+        
     });
+
+    it('should emit "ipChanged" n times', function (done) {
+        var n = 5;
+        var countFive = counter(n, watcher.stop.bind(watcher));
+        extIP.set(null, countFive.invocations);
+
+        watcher.on('ipChanged', function (prevIP, newIP) {
+            expect(newIP).to.equal(countFive.invocations);
+            countFive.increment();
+            extIP.set(null, countFive.invocations);
+        });
+
+        watcher.on('stop', function () {
+            done();
+        });
+
+        watcher.start();
+
+    });
+    // test errors, use error.name
 
 
 });
